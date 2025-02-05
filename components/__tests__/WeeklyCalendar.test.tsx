f5b16f3e05bc8d7bc5c17f804efa1505bc738d65
Scarlett Smith <scarlettsmith12004@gmail.com>
2025-02-05 13:53:49 +0000

    Fix imports

--- components/__tests__/WeeklyCalendar.test.tsx
+++ components/__tests__/WeeklyCalendar.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native"
-import { WeeklyCalendar } from "@tabs/index";
+import { WeeklyCalendar } from "@/app/(tabs)/habits";
 
 describe("WeeklyCalendar", () => {
     test("loads without crashing", () => {
