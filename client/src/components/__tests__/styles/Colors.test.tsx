cad34cf2993d64ee48a11cbb032c0829afbbad21
Ming-Luoyuan <liuziming464@gmail.com>
2025-03-17 17:15:24 +0000

    little changes

--- client/src/components/__tests__/styles/Colors.test.tsx
+++ client/src/components/__tests__/styles/Colors.test.tsx
@@ -2,9 +2,9 @@ import { Colors } from '../../styles/Colors';
 
 describe('Colors module', () => {
   test('dark mode has correct text color', () => {
-    expect(Colors.dark.text).toBe('#ECEDEE'); // 或你期望的颜色值
+    expect(Colors.dark.text).toBe('#ECEDEE');
   });
   test('light mode has correct text color', () => {
-    expect(Colors.light.text).toBe('#11181C'); // 或你期望的颜色值
+    expect(Colors.light.text).toBe('#11181C');
   });
 });
